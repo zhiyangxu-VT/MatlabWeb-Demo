8f7aa9b994a6f48de238bc897c813a539a8cd484
h-bar <663yan@gmail.com>
2017-09-06 18:06:12 -0400

    Python server is working uploading and start mathlab analysis

---
+++ matlab_handler.py
@@ -0,0 +1,46 @@
+#! python
+
+import time
+import os
+
+from flask import Flask
+from flask import request
+from werkzeug.utils import secure_filename
+
+import matlab.engine
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/matlab/<action>', methods=['POST'])
+def handle_matlab(action):
+    saved_files = save_upload_files(request.files)
+    result = matlab_analyse(action, saved_files)
+    return result
+    
+def save_upload_files(uploadfile):
+    a_file = uploadfile['uploadfile']
+    file_path = 'uploads'
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+    time_stamp = str(time.time())
+    file_name = time_stamp + '-' + secure_filename(a_file.filename)
+    
+    temp_file = os.path.join(file_path, file_name)
+    a_file.save(temp_file)
+
+    return temp_file
+
+def matlab_analyse(action, files):
+    eng = matlab.engine.start_matlab()
+    matlab_func = getattr(eng, action)
+    result = matlab_func(files)
+    
+    return str(result)
+
+if __name__ == "__main__":
+    app.run()
