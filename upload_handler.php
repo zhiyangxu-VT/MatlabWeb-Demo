0319d31463ef14fb65b95032dbbb13485ca23f57
h-bar <663yan@gmail.com>
2017-09-06 14:40:52 -0400

    Add other type of file support. Need to figure out how to display text file on html ;

--- upload_handler.php
+++ upload_handler.php
@@ -1,54 +1,31 @@
 <?php
 $uploadOk = 1;
-$imageFileType = pathinfo(basename($_FILES["imageFile"]["name"]),PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["imageFile"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-
+$fileExtension = pathinfo(basename($_FILES["uploadedFile"]["name"]),PATHINFO_EXTENSION);
 // Check file size
-if ($_FILES["imageFile"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
+// if ($_FILES["uploadedFile"]["size"] > 500000) {
+//     echo "Sorry, your file is too large.";
+//     $uploadOk = 0;
+// }
 
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+$date = new DateTime();
+$target_dir = "uploaded_file/";
+$target_file = $target_dir . "file-" . $date->getTimestamp() . "." . $fileExtension;
+
+if(!file_exists($target_dir)) {
+    mkdir($target_dir);
+}
+while (file_exists($target_file)) {
+    $target_file = $target_dir . "file-" . $date->getTimestamp() . "." . $fileExtension;
+}
+if (move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $target_file)) {
+    $target_type = explode("/", mime_content_type($target_file), 2)[0];
+    matlab_analysis($target_file, $target_type);
 } else {
-    $date = new DateTime();
-    $target_dir = "uploaded_img/";
-    $target_file = $target_dir . "img-" . $date->getTimestamp() . "." . $imageFileType;
-    
-    if(!file_exists($target_dir)) {
-        mkdir($target_dir);
-    }
-    while (file_exists($target_file)) {
-        $target_file = $target_dir . "img-" . DateTime::getTimestamp();
-    }
-    if (move_uploaded_file($_FILES["imageFile"]["tmp_name"], $target_file)) {
-        matlab_analysis($target_file);
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+    echo "Sorry, there was an error uploading your file.";
 }
 
 
-function matlab_analysis($filepath) {
+function matlab_analysis($filepath, $filetype) {
     $my_address = "127.0.0.1";
     $matlab_port = 3000;
     $matlab_address = "127.0.0.1";    
@@ -65,7 +42,8 @@ function matlab_analysis($filepath) {
     }
     
     sleep(1);
-    $data = $my_address . "/" . $filepath . "\n";
+    $fileurl = $my_address . "/" . $filepath;
+    $data = json_encode(array("type" => $filetype, "url" => $fileurl)) . "\n";
     socket_write($socket, $data, strlen($data));
     
     while ($response = socket_read($socket, 2048)) {
