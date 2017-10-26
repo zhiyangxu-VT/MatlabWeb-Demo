b22a22a66b7c7cd1209d1829f0637379bcf4256b
h00bar00 <663yan@gmail.com>
2017-10-26 14:58:25 -0400

    Prompt if not run as administrator or root

--- prepare.py
+++ prepare.py
@@ -37,15 +37,20 @@ except:
 
 try:
     import flask
+    flask_status = 0
 except:
     print("Installing flask")
-    pip.main(['install', 'flask'])
-
+    if pip.main(['install', 'flask']) != 0:
+        print('Errow while setting up the enviroment. Plase run as administrator')
+        sys.exit()
 try:
     import werkzeug.utils
+    werkzeug_status = 0
 except:
     print("Installing werkzeug")
-    pip.main(['install', 'werkzeug'])
+    if pip.main(['install', 'werkzeug']) != 0:
+        print('Errow while setting up the enviroment. Plase run as administrator')
+        sys.exit()
 
 try:
     import matlab.engine
@@ -54,6 +59,8 @@ except:
     cwd = matlab_root + matlab_engin
     cmd = 'python setup.py install'
     install_process = subprocess.Popen(cmd, cwd=cwd)
-    install_process.wait()
+    if install_process.wait() != 0:
+        print('Errow while setting up the enviroment. Plase run as administrator')
+        sys.exit()
 
 print("Demo enviroment is ready")
